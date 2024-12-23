ac35bc7cb84909505b34fab4f9dc0ab8e48b0903
Resnicoff <martin.resnicoff@enel.com>
2024-12-23 08:53:59 -0300

    corrigiendo el vite json

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,7 +1,7 @@
 // controllers/uploadController.js
 const path = require('path');
 require('dotenv').config(); 
-
+console.log("file name:",req.file.filename,"URL:",process.env.REACT_APP_API_URL  )
 const uploadFile = (req, res) => {
   // Asumiendo que `req.file` es manejado por Multer o una librería similar para el upload de archivos
   console.log(req.file.filename)
